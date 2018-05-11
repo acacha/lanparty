ebfd6f2bf40b88e1bb4ca238b5180955ba187282
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2018-05-11 16:03:24 +0200

    Sync

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -28,7 +28,7 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 let token = document.head.querySelector('meta[name="csrf-token"]');
 let userHeader = document.head.querySelector('meta[name="user"]')
 window.user = null
-if (userHeader) window.user = JSON.parse(userHeader.content);
+if (userHeader && userHeader.content) window.user = JSON.parse(userHeader.content);
 
 if (token) {
     window.axios.defaults.headers.common['X-CSRF-TOKEN'] = token.content;
