56dce460231eb7f19eb7c74a48b9a7f7efe87d35
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-04-03 17:56:21 +0200

    Sync

--- app/helpers.php
+++ app/helpers.php
@@ -724,7 +724,8 @@ if (!function_exists('manager_permissions')) {
         return [
             'events.index',
             'managers.index',
-            'prizes.index'
+            'prizes.index',
+            'managers.invitation.send'
         ];
     }
 }
