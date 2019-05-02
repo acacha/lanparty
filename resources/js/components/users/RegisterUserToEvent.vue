2b00864da8cd69a314014e9664313300ff088010
Sergi Tur Badenas <sergiturbadenas@gmail.com>
2019-05-02 14:02:22 +0200

    Sync

---
+++ resources/js/components/users/RegisterUserToEvent.vue
@@ -0,0 +1,109 @@
+<template>
+    <span>
+        <v-tooltip bottom>
+            <v-btn icon slot="activator" @click="dialog = true">
+                <v-icon color="teal">add_circle</v-icon>
+            </v-btn>
+            <span>Inscriure l'usuari a un esdeveniment</span>
+        </v-tooltip>
+        <v-dialog v-model="dialog" max-width="750px">
+            <v-card>
+                <v-card-title class="primary darken-3 white--text">
+                    <span>Inscriure a un esdeveniment</span>
+                </v-card-title>
+                <v-card-text>
+                    <v-select
+                            :items="individualInscriptionEvents"
+                            v-model="event"
+                            label="Escolliu un esdeveniment"
+                    >
+                        <template slot="selection" slot-scope="data">
+                            <v-chip class="chip--select">
+                                {{ data.item.name }}
+                            </v-chip>
+                        </template>
+                        <template slot="item" slot-scope="data">
+                            <v-list-tile avatar>
+                                <v-list-tile-avatar>
+                                    <img :src="data.item.image">
+                                </v-list-tile-avatar>
+                                <v-list-tile-content>
+                                     ( Places: {{ data.item.available_tickets }})
+                                </v-list-tile-content>
+                            </v-list-tile>
+                        </template>
+
+
+
+
+
+                        <template slot="item" slot-scope="data">
+                            <template v-if="typeof data.item !== 'object'">
+                                <v-list-tile-content v-text="data.item"></v-list-tile-content>
+                            </template>
+                            <template v-else>
+                                <v-list-tile-avatar>
+                                    <img v-bind:src="gravatarURL(data.item.email)"/>
+                                </v-list-tile-avatar>
+                                <v-list-tile-content>
+                                    <v-list-tile-title>{{data.item.name}} | {{ data.item.givenName }} {{ data.item.sn1 }} {{ data.item.sn2 }}</v-list-tile-title>
+                                    <v-list-tile-sub-title>{{data.item.id}} | {{data.item.email}} | {{ data.item.formatted_created_at_date }}</v-list-tile-sub-title>
+                                </v-list-tile-content>
+                            </template>
+                        </template>
+
+
+
+                    </v-select>
+                </v-card-text>
+                <v-card-actions>
+                    <v-btn color="primary" flat @click.stop="dialog = false">Tancar</v-btn>
+                    <v-btn v-if="!done" :loading="loading" :disabled="loading || event === null" color="primary" @click.stop="registerEvent">Inscriure</v-btn>
+                    <v-btn v-else color="success" flat><v-icon>done</v-icon> Inscrit</v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </span>
+</template>
+
+<script>
+import * as actions from '../../store/action-types'
+import sleep from '../../utils/sleep'
+
+export default {
+  name: 'RegisterUserToEvent',
+  data () {
+    return {
+      dialog: false,
+      loading: false,
+      event: null,
+      done: false
+    }
+  },
+  props: {
+    events: {
+      type: Array,
+      required: true
+    },
+  },
+  computed: {
+    individualInscriptionEvents () {
+      return this.events.filter((event) => {
+        return event.inscription_type_id === '2'
+      })
+    }
+  },
+  methods: {
+    registerEvent () {
+      this.loading = true
+      this.$store.dispatch(actions.REGISTER_USER_TO_EVENT, { user: this.selectedUser, event: this.event }).then((response) => {
+        this.done = true
+        this.loading = false
+        sleep(1000).then(() => { this.dialog = false; this.done = false })
+      }).catch(() => {
+        this.loading = false
+      })
+    }
+  }
+}
+</script>
--- resources/js/components/users/UserInscriptions.vue
+++ resources/js/components/users/UserInscriptions.vue
@@ -37,46 +37,7 @@
 
         <v-card-actions class="white">
             <v-spacer></v-spacer>
-            <v-btn icon slot="activator" @click="registerUserToEvent = true">
-                <v-icon color="teal">add_circle</v-icon>
-            </v-btn>
-            <v-dialog v-model="registerUserToEvent" max-width="750px">
-                <v-card>
-                    <v-card-title class="primary darken-3 white--text">
-                        <span>Inscriure a un esdeveniment</span>
-                    </v-card-title>
-                    <v-card-text>
-                        <v-select
-                                :items="individualInscriptionEvents"
-                                v-model="eventToRegister"
-                                label="Escolliu un esdeveniment"
-                                clearable
-                        >
-                            <template slot="selection" slot-scope="data">
-                                <v-chip class="chip--select">
-                                    {{ data.item.name }}
-                                </v-chip>
-                            </template>
-                            <template slot="item" slot-scope="data">
-                                <v-list-tile avatar>
-                                    <v-list-tile-avatar>
-                                        <img :src="data.item.image">
-                                    </v-list-tile-avatar>
-                                    <v-list-tile-content>
-                                        {{ data.item.name }} ( Places: {{ data.item.available_tickets }})
-                                    </v-list-tile-content>
-                                </v-list-tile>
-                            </template>
-                        </v-select>
-                    </v-card-text>
-                    <v-card-actions>
-                        <v-btn color="primary" flat @click.stop="registerUserToEvent=false">Tancar</v-btn>
-                        <v-btn v-if="!eventRegistered" :loading="registeringEvent" color="primary" @click.stop="registerEvent" :disabled="eventToRegister === null">Inscriure</v-btn>
-                        <v-btn v-else color="success" flat><v-icon>done</v-icon> Inscrit</v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-dialog>
-
+            <register-user-to-event :events="events"></register-user-to-event>
             <unregister-user-from-all-events></unregister-user-from-all-events>
         </v-card-actions>
     </v-card>
@@ -86,19 +47,17 @@
 import { mapGetters } from 'vuex'
 import * as actions from '../../store/action-types'
 import UnregisterUserFromAllEvents from '../users/UnregisterUserFromAllEvents'
+import RegisterUserToEvent from '../users/RegisterUserToEvent'
 export default {
   name: 'UserInscriptions',
   components: {
-    'unregister-user-from-all-events': UnregisterUserFromAllEvents
+    'unregister-user-from-all-events': UnregisterUserFromAllEvents,
+    'register-user-to-event': RegisterUserToEvent
   },
   data () {
     return {
       confirmingUnregisterEvent: null,
-      eventToRegister: null,
-      unregisteringEvent: false,
-      registerUserToEvent: false,
-      eventRegistered: false,
-      registeringEvent: false
+      unregisteringEvent: false
     }
   },
   props: {
@@ -113,11 +72,6 @@ export default {
   },
   computed: {
     ...mapGetters(['selectedUser']),
-    individualInscriptionEvents () {
-      return this.events.filter((event) => {
-        return event.inscription_type_id === '2'
-      })
-    },
     currentEvents () {
       if (this.selectedUser.events) return this.selectedUser.events.filter(event => event.session === this.session)
       return this.selectedUser.events
@@ -138,16 +92,6 @@ export default {
       }).catch(() => {
         this.unregisteringEvent = false
         this.confirmingUnregisterEvent = null
-      })
-    },
-    registerEvent () {
-      this.registeringEvent = true
-      this.$store.dispatch(actions.REGISTER_USER_TO_EVENT, { user: this.selectedUser, event: this.eventToRegister }).then((response) => {
-        this.eventRegistered = true
-        this.registeringEvent = false
-        sleep(1000).then(() => { this.registerUserToEvent = false; this.eventRegistered = false })
-      }).catch(() => {
-        this.registeringEvent = false
       })
     }
   }
