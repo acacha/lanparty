d02cc73712bac68aaef09ba8b0da2b6f7a832e1f
SergiBaucells <sergibaucells@iesebre.com>
2019-05-15 17:24:29 +0200

    Changed formatted dates

--- resources/js/components/events/EventsList.vue
+++ resources/js/components/events/EventsList.vue
@@ -90,17 +90,17 @@
             <td>
               {{ event.participants_number }}
             </td>
-            <td>
-              {{ event.published_at }}
+            <td :title="event.formatted_published_at">
+              {{ event.formatted_published_at_diff }}
             </td>
-            <td>
-              {{ event.deleted_at }}
+            <td :title="event.formatted_deleted_at">
+              {{ event.formatted_deleted_at_diff }}
             </td>
-            <td>
-              {{ event.created_at }}
+            <td :title="event.formatted_created_at">
+              {{ event.formatted_created_at_diff }}
             </td>
-            <td>
-              {{ event.updated_at }}
+            <td :title="event.formatted_updated_at">
+              {{ event.formatted_updated_at_diff }}
             </td>
             <td class="d-flex">
                 <event-show :event="event"></event-show>
@@ -157,10 +157,10 @@ export default {
         { text: 'Imatge', value: 'image' },
         { text: 'Reglament', value: 'regulation' },
         { text: 'Número de participants', value: 'participants_number' },
-        { text: 'Publicat', value: 'published_at' },
-        { text: 'Esborrat', value: 'deleted_at' },
-        { text: 'Creat', value: 'created_at' },
-        { text: 'Modificat', value: 'updated_at' },
+        { text: 'Publicat', value: 'published_at_timestamp' },
+        { text: 'Esborrat', value: 'deleted_at_timestamp' },
+        { text: 'Creat', value: 'created_at_timestamp' },
+        { text: 'Modificat', value: 'updated_at_timestamp' },
         { text: 'Accions', sortable: false, value: 'full_search' }
       ]
     }
