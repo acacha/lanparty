c38ed5670ee3776b58b148760352e76a63bf6d2d
benjaminzaragozacd <benjaminzaragoza@iesebre.com>
2019-05-15 18:24:30 +0200

    a

--- tests/Feature/Api/PrizesControllerTest.php
+++ tests/Feature/Api/PrizesControllerTest.php
@@ -129,8 +129,148 @@ class PrizesControllerTest extends TestCase
         $this->assertEquals(1,$prize->partner_id);
         $this->assertEquals(1,$prize->value);
         $this->assertEquals(true,$prize->multiple);
+    }
+    /** @test */
+    public function manager_can_create_prizes()
+    {
+        $this->loginAsManager('api');
 
+        $response = $this->json('POST','/api/v1/prizes/',[
+            'name' => 'WII',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
 
+        $result = json_decode($response->getContent());
+        $response->assertSuccessful();
+
+        $this->assertNotNull($prize = Prize::find($result->id));
+        $this->assertEquals('WII',$result->name);
+        $this->assertEquals('bo',$result->description);
+        $this->assertEquals('bo',$result->notes);
+        $this->assertEquals(1,$result->partner_id);
+        $this->assertEquals(1,$result->value);
+        $this->assertEquals(true,$result->multiple);
+
+        $this->assertEquals('WII',$prize->name);
+        $this->assertEquals('bo',$prize->description);
+        $this->assertEquals('bo',$prize->notes);
+        $this->assertEquals(1,$prize->partner_id);
+        $this->assertEquals(1,$prize->value);
+        $this->assertEquals(true,$prize->multiple);
+    }
+    /** @test */
+    public function regular_user_cannot_create_prizes()
+    {
+        $this->login('api');
+
+        $response = $this->json('POST','/api/v1/prizes/',[
+            'name' => 'WII',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
+
+        json_decode($response->getContent());
+        $response->assertStatus(403);
+    }
+    /** @test */
+    public function guest_user_cannot_create_prizes()
+    {
+        $response = $this->json('POST','/api/v1/prizes/',[
+            'name' => 'WII',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
+
+        json_decode($response->getContent());
+        $response->assertStatus(401);
+
+    }
+    /** @test */
+    public function superadmin_can_edit_prize()
+    {
+        $this->loginAsSuperAdmin('api');
+
+        $this->withoutExceptionHandling();
+        $oldPrize =factory(Prize::class)->create([
+            'name' => 'WII',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
+
+        $response = $this->json('PUT','/api/v1/prizes/'.$oldPrize->id,[
+            'name' => 'PSP',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
+
+        $result =json_decode($response->getContent());
+        $response->assertSuccessful();
+
+        $newPrize =  $oldPrize->refresh();
+        $this->assertNotNull($newPrize);
+        $this->assertEquals('PSP', $result->name);
+    }
+    /** @test */
+    public function manager_can_edit_prize()
+    {
+        $this->loginAsManager('api');
+
+        $oldPrize =factory(Prize::class)->create([
+            'name' => 'WII',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
+
+        $response = $this->json('PUT','/api/v1/prizes/'.$oldPrize->id,[
+            'name' => 'PSP',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
+
+        $result =json_decode($response->getContent());
+        $response->assertSuccessful();
+
+        $newPrize =  $oldPrize->refresh();
+        $this->assertNotNull($newPrize);
+        $this->assertEquals('PSP', $result->name);
+    }
+
+    /** @test */
+    public function cannot_edit_prize_without_name()
+    {
+        $this->loginAsManager('api');
+        $oldPrize = factory(Prize::class)->create();
+        $response = $this->json('PUT','/api/v1/prizes/'.$oldPrize->id,[
+            'name' => '',
+            'description' => 'bo',
+            'notes' => 'bo',
+            'partner_id' => 1,
+            'value' => 1,
+            'multiple' => true,
+        ]);
+        $response->assertStatus(422);
     }
 }
 
